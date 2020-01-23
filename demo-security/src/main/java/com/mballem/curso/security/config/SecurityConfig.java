5283f0ddae14fc214f26e688e6d76e4c3d2b6f3e
05721985151 <05721985151@host.docker.internal>
2020-01-23 14:36:34 -0200

    aula 17

--- demo-security/src/main/java/com/mballem/curso/security/config/SecurityConfig.java
+++ demo-security/src/main/java/com/mballem/curso/security/config/SecurityConfig.java
@@ -18,7 +18,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/webjars/**", "/css/**", "/image/**").permitAll()
-				.antMatchers("/", "/home").permitAll().anyRequest().authenticated().and().formLogin()
+				.antMatchers("/", "/home").permitAll()
+				//acesso privaod admin				
+				.antMatchers("/u/**").hasAuthority("ADMIN")				
+				//acesso medico
+				.antMatchers("/medicos/**").hasAuthority("MEDICO")		
+				.anyRequest().authenticated().and().formLogin()
 				.loginPage("/login").defaultSuccessUrl("/", true).failureUrl("/login-error").permitAll().and().logout()
 				.logoutSuccessUrl("/");
 	}
